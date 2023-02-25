4fd0434fe43eda9ae819e6d74eaed82769c3a005
achaivre <alyx.chaivre@gmail.com>
2023-02-25 14:37:37 -0600

    added julia folder and stuff to ashley's

--- ashley/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views as v
-
-urlpatterns = [
-    path("", v.root, name="main"),
-    path("tasklist/", v.TaskList.as_view(), name="task_list"),
-    path("task/<int:pk>", v.TaskDetail.as_view(), name="task_detail"),
-]
