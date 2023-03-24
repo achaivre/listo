1cca7b0df13fe8e49eb4bfdffa30b82e1e099979
Ashley Glasz <ashleybglasz@gmail.com>
2023-03-24 15:24:35 -0500

    spacing edits

--- alyx/config/urls.py
+++ alyx/config/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 from listo.views import *
 
@@ -47,3 +48,5 @@ urlpatterns = [
     ),
     path("logout", logout_view, name="logout"),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
