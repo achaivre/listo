76cf4964d368b27d9dbe9ade9621dda7cc71405c
Decodedmind <115696970+Decodedmind@users.noreply.github.com>
2023-03-23 19:08:59 -0500

    commit

--- eddie/eddie_spanish_attempt/todolist/urls.py
+++ eddie/eddie_spanish_attempt/todolist/urls.py
@@ -10,6 +10,16 @@ from .views import (
     HighPriority,
     LowPriority,
     MediumPriority,
+    Sp_LoginView_ToDo,
+    Sp_CreateUserPage,
+    Sp_AllTasks,
+    Sp_Completed_View,
+    Sp_TaskCreate,
+    Sp_TaskUpdate,
+    Sp_TaskDelete,
+    Sp_HighPriority,
+    Sp_LowPriority,
+    Sp_MediumPriority,
 )
 from django.contrib.auth.views import LogoutView
 
@@ -27,4 +37,19 @@ urlpatterns = [
     path("task-create/", TaskCreate.as_view(), name="create_task"),
     path("task-update/<int:pk>/", TaskUpdate.as_view(), name="update_task"),
     path("task-delete/<int:pk>/", TaskDelete.as_view(), name="delete_task"),
+    # spanish urls
+    # spanish urls
+    path("accounts/slogin/", Sp_LoginView_ToDo.as_view(), name="slogin"),
+    path("slogout/", LogoutView.as_view(next_page="login"), name="slogout"),
+    path("sregister/", CreateUserPage.as_view(), name="sregister"),
+    # Show Tasks Pages
+    path("", AllTasks.as_view(), name="all_tasks"),
+    path("scompleted-tasks/", Sp_Completed_View.as_view(), name="scompleted_tasks"),
+    path("shigh-priority-tasks/", Sp_HighPriority.as_view(), name="shigh_tasks"),
+    path("smed-priority-tasks/", Sp_MediumPriority.as_view(), name="smedium_tasks"),
+    path("slow-priority-tasks/", Sp_LowPriority.as_view(), name="slow_tasks"),
+    # Task Creation/Editing Pages
+    path("stask-create/", Sp_TaskCreate.as_view(), name="screate_task"),
+    path("stask-update/<int:pk>/", Sp_TaskUpdate.as_view(), name="supdate_task"),
+    path("stask-delete/<int:pk>/", Sp_TaskDelete.as_view(), name="sdelete_task"),
 ]
--- eddie/eddie_spanish_attempt/todolist/views.py
+++ eddie/eddie_spanish_attempt/todolist/views.py
@@ -148,7 +148,7 @@ class TaskCreate(LoginRequiredMixin, CreateView):
 
 
 class Sp_LoginView_ToDo(LoginView):
-    template_name = "spanish/login.html"
+    template_name = "spanish/slogin.html"
     fields = "__all__"
     redirect_authenticated_user = True
 
@@ -157,7 +157,7 @@ class Sp_LoginView_ToDo(LoginView):
 
 
 class Sp_CreateUserPage(FormView):
-    template_name = "spanish/register.html"
+    template_name = "spanish/sregister.html"
     form_class = UserCreationForm
     redirect_authenticated_user = True
     success_url = reverse_lazy("all_tasks")
@@ -180,7 +180,7 @@ class Sp_CreateUserPage(FormView):
 class Sp_AllTasks(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = "tasks"
-    template_name = "spanish/all_tasks.html"
+    template_name = "spanish/sall_tasks.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -192,7 +192,7 @@ class Sp_AllTasks(LoginRequiredMixin, ListView):
 class Sp_HighPriority(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = "high_tasks"
-    template_name = "spanish/high_tasks.html"
+    template_name = "spanish/shigh_tasks.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -206,7 +206,7 @@ class Sp_HighPriority(LoginRequiredMixin, ListView):
 class Sp_MediumPriority(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = "med_tasks"
-    template_name = "spanish/med_tasks.html"
+    template_name = "spanish/smed_tasks.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -220,7 +220,7 @@ class Sp_MediumPriority(LoginRequiredMixin, ListView):
 class Sp_LowPriority(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = "low_tasks"
-    template_name = "spanish/low_tasks.html"
+    template_name = "spanish/slow_tasks.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -234,7 +234,7 @@ class Sp_LowPriority(LoginRequiredMixin, ListView):
 class Sp_Completed_View(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = "finished_tasks"
-    template_name = "spanish/completed_tasks.html"
+    template_name = "spanish/scompleted_tasks.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -248,14 +248,14 @@ class Sp_Completed_View(LoginRequiredMixin, ListView):
 # Task Edit/Create/Delete Pages
 class Sp_TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    template_name = "spanish/task_form.html"
+    template_name = "spanish/stask_form.html"
     fields = ["title", "priority", "is_complete"]
     success_url = reverse_lazy("all_tasks")
 
 
 class Sp_TaskDelete(LoginRequiredMixin, DeleteView):
     model = Task
-    template_name = "spanish/task_form.html"
+    template_name = "spanish/stask_form.html"
     context_object_name = "task"
     success_url = reverse_lazy("all_tasks")
 
@@ -267,9 +267,36 @@ class Sp_TaskDelete(LoginRequiredMixin, DeleteView):
 class Sp_TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
     form_class = TaskForm
-    template_name = "spanish/task_form.html"
+    template_name = "spanish/stask_form.html"
     success_url = reverse_lazy("all_tasks")
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(TaskCreate, self).form_valid(form)
+
+
+class Sp_LoginView_ToDo(LoginView):
+    template_name = "spanish/slogin.html"
+    fields = "__all__"
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy("all_tasks")
+
+
+class Sp_CreateUserPage(FormView):
+    template_name = "spanish/sregister.html"
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy("all_tasks")
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(CreateUserPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect("all_tasks")
+        return super(CreateUserPage, self).get(*args, **kwargs)
